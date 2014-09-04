46dee23c44b6d1a49e5f1f10796a49ab14541efb
holidayp <holidayp@uklon-06689>
2014-09-04 18:13:09 +0100

    Markup for Tags is now created in HeroTag

--- src/java/com/lbi/localheroes/tags/HeroTag.java
+++ src/java/com/lbi/localheroes/tags/HeroTag.java
@@ -25,47 +25,70 @@ import org.json.simple.parser.ParseException;
  * @author holidayp
  */
 public class HeroTag extends TagSupport{
+    
+    private static final String CATEGORY_ATTRIBUTE_NAME = "Category";
+    private static final String HEROES_ATTRIBUTE_NAME = "Heroes";
+    private static final String TAGS_ATTRIBUTE_NAME = "Tags";
+    
+    private static final String REST_URI = "http://localhost:8080/MVC/rest/";
+    private static final String GET_REQUEST_METHOD = "GET";
+    private static final String REQUEST_PROPERTY = "ACCEPT";
+    private static final String REQUEST_PROPERTY_VALUE = "application/json";
+    private static final String HTTP_ERROR_STRING = "Failed : HTTP error code : ";
+    
+    private static final String HEROES_CONTENT_TYPE = "heroes";
+    private static final String TAGS_CONTENT_TYPE = "tags";
+    
+    private static final String RESULT = "result";
+    
+    private static final String ADDRESS = "address";
+    private static final String NAME = "name";
+    private static final String LINE1 = "line1";
+    private static final String COUNTY = "county";
+    private static final String POSTCODE = "postCode";
+    
+    private static final String RESULTS_DIV = "<div class=\"result\" id=\"result-";
+    private static final String CLOSE_TAG = "\">";
+    private static final String HEADER_3_TAG = "<h3>";
+    private static final String CLOSE_HEADER_3_TAG = "</h3>";
+    private static final String PARAGRAPH_TAG = "<p>";
+    private static final String COMMA = ", ";
+    private static final String CLOSE_PARAGRAPH_TAG = "</p>";
+    private static final String CLOSE_DIV_TAG = "</div>";
+    private static final String TAG_SPAN = "<span class=\"tag\">";
+    private static final String END_SPAN = "</span>";
+    
+    private static final String FORWARD_SLASH = "/";
+    
+    
   
     @Override
     public int doEndTag() throws JspException {
 
-        String category = pageContext.getRequest().getParameter("Category");
+        String category = pageContext.getRequest().getParameter(CATEGORY_ATTRIBUTE_NAME);
         
-        pageContext.setAttribute("Category", category);
+        pageContext.setAttribute(CATEGORY_ATTRIBUTE_NAME, category);
+        pageContext.setAttribute(HEROES_ATTRIBUTE_NAME, getHeroesByCategory(category));
+        pageContext.setAttribute(TAGS_ATTRIBUTE_NAME, getTagsByCategory(category));
+        
+
+        return EVAL_PAGE;
+    }
+    
+    private String getHeroesByCategory(String category) {
+        String markupForHeroes = null;
         
         try {
-            URL url = new URL("http://localhost:8080/MVC/rest/heroes/" + category);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
 
-            if (conn.getResponseCode() != 200) {
-                    throw new RuntimeException("Failed : HTTP error code : "
-                                    + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                    System.out.println(output);
-                    JSONArray jsonArray = (JSONArray) new JSONParser().parse(output);
-                    output = getMarkupForHeroes(jsonArray);
-                    pageContext.setAttribute("Heroes", output);
-            }
-
-            conn.disconnect();
+            String output = getOutputFromRestServiceCall(HEROES_CONTENT_TYPE, category);
+            JSONArray jsonArray = (JSONArray) new JSONParser().parse(output);
+            markupForHeroes = getMarkupForHeroes(jsonArray);
             
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(HeroTag.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(HeroTag.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ParseException ex) {
             Logger.getLogger(HeroTag.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        return EVAL_PAGE;
+        
+        return markupForHeroes;
     }
     
     
@@ -76,23 +99,23 @@ public class HeroTag extends TagSupport{
         for (int i = 0; i < jsonArray.size(); i++) {
             json = (JSONObject) jsonArray.get(i);
             
-            markupString.append("<div class=\"result\" id=\"result-");
+            markupString.append(RESULTS_DIV);
             markupString.append(i);
-            markupString.append("\">");
-            markupString.append("<h3>");
-            markupString.append(json.get("name"));
-            markupString.append("</h3>");
+            markupString.append(END_SPAN);
+            markupString.append(HEADER_3_TAG);
+            markupString.append(json.get(NAME));
+            markupString.append(CLOSE_HEADER_3_TAG);
             
-            if(json.get("address") != null) {
+            if(json.get(ADDRESS) != null) {
                 try {
-                    JSONObject jsonAddress = (JSONObject) new JSONParser().parse(json.get("address").toString());
-                    markupString.append("<p>");
-                    markupString.append(jsonAddress.get("line1"));
-                    markupString.append(", ");
-                    markupString.append(jsonAddress.get("county"));
-                    markupString.append(", ");
-                    markupString.append(jsonAddress.get("postCode"));
-                    markupString.append("</p>");
+                    JSONObject jsonAddress = (JSONObject) new JSONParser().parse(json.get(ADDRESS).toString());
+                    markupString.append(PARAGRAPH_TAG);
+                    markupString.append(jsonAddress.get(LINE1));
+                    markupString.append(COMMA);
+                    markupString.append(jsonAddress.get(COUNTY));
+                    markupString.append(COMMA);
+                    markupString.append(jsonAddress.get(POSTCODE));
+                    markupString.append(CLOSE_PARAGRAPH_TAG);
                 } catch (ParseException ex) {
                     Logger.getLogger(HeroTag.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -100,14 +123,79 @@ public class HeroTag extends TagSupport{
             }
             
             
-            markupString.append("</div>");
+            markupString.append(CLOSE_DIV_TAG);
         }
         
         return markupString.toString();
     }
     
     
+    private String getTagsByCategory(String category) {
+        String markupForTags = null;
+        
+        try {
+            
+            String output = getOutputFromRestServiceCall(TAGS_CONTENT_TYPE, category);
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(output);
+            markupForTags = getMarkupForTags(jsonObject);
 
+        } catch (ParseException ex) {
+            Logger.getLogger(HeroTag.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return markupForTags;
+    }
+    
+    private String getMarkupForTags (JSONObject jsonObject) {
+        StringBuilder markupString = new StringBuilder();
+
+        JSONArray jsonResults = null;
+        
+        try {
+            jsonResults = (JSONArray) new JSONParser().parse(jsonObject.get(RESULT).toString());
+            
+            JSONObject json = null;
+            for (int i = 0; i < jsonResults.size(); i++) {
+                json = (JSONObject) jsonResults.get(i);
+                markupString.append(TAG_SPAN);
+                markupString.append(json.get(NAME));
+                markupString.append(END_SPAN);
+            
+            }
+        }catch (ParseException ex) {
+            Logger.getLogger(HeroTag.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return markupString.toString();
+    }
+    
+    private String getOutputFromRestServiceCall(String contentType, String category) {
+        String restServiceOutput = null;
+        
+        try {
+            URL url = new URL(REST_URI + contentType + FORWARD_SLASH + category);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(GET_REQUEST_METHOD);
+            conn.setRequestProperty(REQUEST_PROPERTY, REQUEST_PROPERTY_VALUE);
+
+            if (conn.getResponseCode() != 200) {
+                    throw new RuntimeException(HTTP_ERROR_STRING
+                                    + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            restServiceOutput = br.readLine();
+            
+            conn.disconnect();
+            
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(HeroTag.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(HeroTag.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return restServiceOutput;
+    }
     
     
 }
--- web/results.jsp
+++ web/results.jsp
@@ -1,16 +1,3 @@
-<%@page import="com.lbi.localheroes.model.Hero"%>
-<%@page import="com.lbi.localheroes.DBQuery"%>
-<%@page import="java.util.ArrayList"%>
-<%@page import="com.mongodb.AggregationOutput"%>
-<%@page import="java.util.Iterator"%>
-<%@page import="org.json.simple.JSONArray"%>
-<%@page import="org.json.simple.parser.JSONParser"%>
-<%@page import="org.json.simple.JSONObject"%>
-<%@page import="com.mongodb.DBCursor"%>
-<%@page import="com.mongodb.BasicDBObject"%>
-<%@page import="com.mongodb.DBObject"%>
-<%@page import="com.mongodb.DBCollection"%>
-<%@page import="com.lbi.localheroes.DBConnector"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@taglib prefix="hero" uri="WEB-INF/tlds/Hero.tld"%>
@@ -44,121 +31,17 @@
             
             <h1>Results for ${pageContext.getAttribute("Category")}</h1>
             <div id="map-canvas"></div>
-            <%--
-                String category = request.getParameter("Category");
-
-                DBQuery dbQuery = new DBQuery("heroes");
-            --%>
             <div class="leftResultsTab">
                 <div class="scrollableResults">
                     //TODO: call ajax to get results and query with url parameters category and tags
-                    <%--<%@include file="/WEB-INF/jspf/resultsList.jspf" %>--%>
                     ${pageContext.getAttribute("Heroes")}
                 </div>
             </div>
             <div class="tags">
                 <h3>Refine your search by selecting tags:</h3><br>
+                ${pageContext.getAttribute("Tags")}
             </div>
         </div>
-        
-       <script type="text/javascript">
-
-            //$('.tag').click(function() {
-            //    $(this).addClass("selected");
-            //    selectedTags.push($(this).html());
-//                doAjax();
-//                function doAjax() {
-//                    $.ajax({
-//                        url: '/Results?Category=' + ${pageContext.getAttribute("Category")} + '&Tags=' + selectedTags,
-//                        success: function(response) {
-//                            //$('.scrollableResults').load('/WEB-INF/jspf/resultsList.jspf');
-//                            $('.scrollableResults').html(response);
-//                        }
-//                    });
-//                }
-                
-//                getResults();
-//                function getResults() {
-//                    var titleString = $('.body h1').html();
-//                    var categoryString = titleString.substring(12); //hard-coded value to get Category
-//                    var urlString = 'rest/heroes/' + categoryString;
-//                    
-//                    
-//                    $.getJSON(urlString, function(data) {
-////                        var count = 0;
-////                        outputResults(jd, count);
-////                        function outputResults(arg, count) {
-//                        $.each(data, function(idx, obj) {
-//                            console.log(obj);
-//                            $('.scrollableResults').append('<div class="result" id="result-' + idx +'"><h3>' + obj.name + '</h3></div>');
-//                            if(obj.address !== null) {
-//                                $('#result-' + idx).append('<p>' + obj.address.line1 + ', ' + obj.address.county + ', ' + obj.address.postCode + '</p>');
-//                            }
-//                            
-//                            var lat = obj.point.latitude;
-//                            var long = obj.point.longitude;
-//
-//                            coordsList.push(lat, long);
-//
-//                        });
-////                            if (arg instanceof Object) {
-////                                Object.keys(arg).forEach(function(key) {
-//////                                    if(typeof arg.name !== 'undefined') {
-//////                                        $('.result').append('<h3>' + arg.name + '</h3>');
-//////                                        $('.result').append('<p>');
-//////                                    }
-////                                    
-////
-////                                        doIt(arg[key]);
-////                                    
-////                                    
-//////                                    console.log(arg.name + ', ' + arg.county);
-//////                                    if(typeof arg !== 'undefined') {
-//////                                        outputResults(arg[key]);
-//////                                    }
-//////                                    else {
-//////                                        $('.result').html('<h3>' + arg.name + '</h3>');
-//////                                        if(arg.address !== null) {
-//////                                            $('.result').append('<p>' + arg.address + '</p> ');
-//////                                            $('.result').append(arg.county + ', ');
-//////                                            $('.result').append(arg.postCode + '</p>');
-//////                                        }
-//////                                    }
-//////                                    console.log(arg);
-////                                    
-////                                });     
-////                            }
-////                            else if (typeof arg === "string") {
-////                                count++;
-////                                console.log(count);
-////                                console.log(arg);
-////                                $('.result').append('<p>' + arg + ', ');
-////                                
-////                            }
-////                            
-////                        }
-////                        
-////                        
-////                        function doIt(heroInfo) {
-////                            if (typeof heroInfo === "string") {
-////
-////                                    console.log(heroInfo);
-////                                    $('.result').append('<p>' + heroInfo + '</p>');
-////
-////                            }
-////                         }
-//                        
-//                        
-//                        
-//                     });
-//                     
-//                     
-//                     
-//
-//                }
-                
-            //});
-        </script>
         
     </body>
 </html>
