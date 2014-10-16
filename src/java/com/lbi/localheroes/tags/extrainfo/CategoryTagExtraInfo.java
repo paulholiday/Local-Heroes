f576325e55ce6cb73e43d2be6f17bbeec826444f
jd <jd@192.168.31.26>
2014-10-16 14:38:03 +0100

    - CategoryTag now returning List<Category> object so that HTML markup can be taken out of Java

--- src/java/com/lbi/localheroes/tags/extrainfo/CategoryTagExtraInfo.java
+++ src/java/com/lbi/localheroes/tags/extrainfo/CategoryTagExtraInfo.java
@@ -1,5 +1,7 @@
 package com.lbi.localheroes.tags.extrainfo;
 
+import com.lbi.localheroes.model.Category;
+import java.util.List;
 import javax.servlet.jsp.tagext.TagData;
 import javax.servlet.jsp.tagext.TagExtraInfo;
 import javax.servlet.jsp.tagext.VariableInfo;
@@ -12,7 +14,7 @@ public class CategoryTagExtraInfo extends TagExtraInfo{
     
     @Override
     public VariableInfo[] getVariableInfo(TagData data) {
-        return new VariableInfo[] { new VariableInfo("Categories", "String", true, VariableInfo.AT_END)};
+        return new VariableInfo[] { new VariableInfo("Categories", List.class.getName(), true, VariableInfo.AT_END)};
     }
     
 }
